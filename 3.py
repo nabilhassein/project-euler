57eef73e8de4ac783ec7afff50a8e7b7df6185d1
Nabil Hassein <nabil.hassein@gmail.com>
2013-08-09 15:57:13 -0400

    added problem statements to top of files as comments; minor change to problem 2; commentary on solution process in problem 3

--- 3.py
+++ 3.py
@@ -1,9 +1,20 @@
+# Largest prime factor
+# Problem 3
+
+# The prime factors of 13195 are 5, 7, 13 and 29.
+
+# What is the largest prime factor of the number 600851475143?
+
+### END PROBLEM STATEMENT; BEGIN MY COMMENTARY
+
 # This is the first result I found by searching the Web via Google for
 # "factor prime number python": 
 # http://stackoverflow.com/questions/15347174/python-finding-prime-factors
-# It was so short that I had already internalized it by the time I could think to look away,
-# and it was so elegant that it seemed silly to look for another solution.
-# So I merely altered it slightly. The other solutions are my original work.
+# It was so short that I had already internalized it by the time I could think
+# to look away, and it was so elegant that it was pointless to try to write
+# another solution. So I merely altered it slightly.
+# The other solutions are my original work,
+# except where noted (nowhere other than here at the time of writing).
 
 def problem3(n):
   i = 2
