512a038f380aa568d951b0ea578a86c7daef86e5
Nabil Hassein <nabil.hassein@gmail.com>
2013-08-10 10:04:38 -0400

    changed problem 1 to use a list comprehension

--- 1.py
+++ 1.py
@@ -7,10 +7,6 @@
 
 
 def problem1(): 
-  sum = 0
-  for i in range(0, 1000):
-    if i % 3 == 0 or i % 5 == 0:
-      sum += i
-  return sum
+  return sum([i for i in range(0, 1000) if i % 3 == 0 or i % 5 == 0])
 
 print problem1()
